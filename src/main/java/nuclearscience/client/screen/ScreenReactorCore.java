563e1aa5c535c7aed94d3363ea56b592ee73e1b4
aurilisdev <aurilisdev@gmail.com>
2021-03-03 22:31:56 +0100

    Fix antimatter grams

--- src/main/java/nuclearscience/client/screen/ScreenReactorCore.java
+++ src/main/java/nuclearscience/client/screen/ScreenReactorCore.java
@@ -32,8 +32,9 @@ public class ScreenReactorCore extends GenericContainerScreen<ContainerReactorCo
 	super.drawGuiContainerForegroundLayer(matrixStack, mouseX, mouseY);
 	font.func_243248_b(matrixStack, new TranslationTextComponent("gui.reactorcore.deuterium"), titleX,
 		(float) titleY + 14, 4210752);
-	font.func_243248_b(matrixStack, new TranslationTextComponent("gui.reactorcore.temperature",
-		container.getTemperature() / 4 + 15 + " C"), titleX, (float) titleY + 14 * 3, 4210752);
+	font.func_243248_b(matrixStack,
+		new TranslationTextComponent("gui.reactorcore.temperature", container.getTemperature() / 4 + 15 + " C"),
+		titleX, (float) titleY + 14 * 3, 4210752);
 	if (container.getTemperature() > TileReactorCore.MELTDOWN_TEMPERATURE_ACTUAL) {
 	    if (System.currentTimeMillis() % 1000 < 500) {
 		font.func_243248_b(matrixStack, new TranslationTextComponent("gui.reactorcore.warning"), titleX,
