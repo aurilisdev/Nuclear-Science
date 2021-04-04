dc464d950137ebb7c39298fe89555a41be0afb74
aurilisdev <aurilisdev@gmail.com>
2021-04-04 23:58:07 +0200

    Update output slot

--- src/main/java/nuclearscience/common/inventory/container/ContainerGasCentrifuge.java
+++ src/main/java/nuclearscience/common/inventory/container/ContainerGasCentrifuge.java
@@ -1,13 +1,13 @@
 package nuclearscience.common.inventory.container;
 
 import electrodynamics.common.inventory.container.GenericContainer;
-import electrodynamics.common.inventory.container.slot.GenericSlot;
 import electrodynamics.common.inventory.container.slot.SlotRestricted;
 import electrodynamics.common.item.subtype.SubtypeProcessorUpgrade;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.inventory.container.FurnaceResultSlot;
 import net.minecraft.util.IIntArray;
 import net.minecraft.util.IntArray;
 import nuclearscience.DeferredRegisters;
@@ -29,8 +29,8 @@ public class ContainerGasCentrifuge extends GenericContainer<TileGasCentrifuge> 
 
     @Override
     public void addInventorySlots(IInventory inv, PlayerInventory playerinv) {
-	addSlot(new GenericSlot(inv, nextIndex(), 126, 21));
-	addSlot(new GenericSlot(inv, nextIndex(), 126, 48));
+	addSlot(new FurnaceResultSlot(playerinv.player, inv, nextIndex(), 126, 21));
+	addSlot(new FurnaceResultSlot(playerinv.player, inv, nextIndex(), 126, 48));
 	addSlot(new SlotRestricted(inv, nextIndex(), 186, 14,
 		electrodynamics.DeferredRegisters.SUBTYPEITEM_MAPPINGS.get(SubtypeProcessorUpgrade.basicspeed),
 		electrodynamics.DeferredRegisters.SUBTYPEITEM_MAPPINGS.get(SubtypeProcessorUpgrade.advancedspeed)));
