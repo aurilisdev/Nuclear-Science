ff9c0e790f8eef33c965dbfa64706719e5b0833f
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2021-10-04 11:40:04 +0200

    Start some basic work on the radioactive grass

---
+++ src/main/java/nuclearscience/common/block/BlockRadioactiveGrass.java
@@ -0,0 +1,29 @@
+package nuclearscience.common.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.SpreadableSnowyDirtBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import nuclearscience.api.radiation.EffectRadiation;
+
+public class BlockRadioactiveGrass extends SpreadableSnowyDirtBlock {
+
+    public BlockRadioactiveGrass() {
+	super((AbstractBlock.Properties.create(Material.ORGANIC, MaterialColor.GREEN).tickRandomly()
+		.hardnessAndResistance(0.6F).sound(SoundType.PLANT)));
+    }
+
+    @Override
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+	if (entityIn instanceof LivingEntity) {
+	    LivingEntity living = (LivingEntity) entityIn;
+	    living.addPotionEffect(new EffectInstance(EffectRadiation.INSTANCE, 20));
+	}
+    }
+}
