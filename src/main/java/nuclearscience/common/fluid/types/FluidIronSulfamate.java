36b0e3084c4f181c83f1f15cb485bfd34729d99d
skip999 <skiprocks999@gmail.com>
2021-11-16 20:14:16 -0600

    Added fluid tag support
    "There, now it's art"

--- src/main/java/nuclearscience/common/fluid/types/FluidIronSulfamate.java
+++ src/main/java/nuclearscience/common/fluid/types/FluidIronSulfamate.java
@@ -4,6 +4,8 @@ import electrodynamics.common.fluid.FluidNonPlaceable;
 
 public class FluidIronSulfamate extends FluidNonPlaceable {
 
+	public static final String FORGE_TAG = "iron_sulfamate";
+	
     public FluidIronSulfamate() {
 	super(() -> electrodynamics.DeferredRegisters.ITEM_CANISTERREINFORCED, nuclearscience.References.ID, "ironsulfamate");
     }
