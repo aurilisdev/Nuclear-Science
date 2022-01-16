23bafc93358325b331d0ff46585354de4cfdfd96
aurilisdev <aurilisdev@gmail.com>
2022-01-16 01:01:39 +0100

    Fix render

--- src/main/java/nuclearscience/client/render/tile/RenderQuantumCapacitor.java
+++ src/main/java/nuclearscience/client/render/tile/RenderQuantumCapacitor.java
@@ -1,8 +1,9 @@
 package nuclearscience.client.render.tile;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Vector3f;
 
+import electrodynamics.prefab.tile.components.ComponentType;
+import electrodynamics.prefab.tile.components.type.ComponentTickable;
 import electrodynamics.prefab.utilities.RenderingUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -19,19 +20,18 @@ public class RenderQuantumCapacitor implements BlockEntityRenderer<TileQuantumCa
 			int combinedLightIn, int combinedOverlayIn) {
 		matrixStackIn.pushPose();
 		matrixStackIn.translate(0.5, 0.5, 0.5);
-		matrixStackIn.mulPoseMatrix(matrixStackIn.last().pose());
-		matrixStackIn.mulPose(Minecraft.getInstance().getEntityRenderDispatcher().cameraOrientation());
-		matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(180.0F));
-		float scale = 0.005f;
+		float scale = (float) Math
+				.abs(Math.sin((tileEntityIn.<ComponentTickable>getComponent(ComponentType.Tickable).getTicks() + partialTicks) / 40.0)) * 0.001f
+				+ 0.001f;
 		matrixStackIn.scale(scale, scale, scale);
 		float distance = (float) Math.sqrt(1 + Minecraft.getInstance().player.distanceToSqr(tileEntityIn.getBlockPos().getX() + 0.5,
 				tileEntityIn.getBlockPos().getY() + 0.5, tileEntityIn.getBlockPos().getZ() + 0.5));
-		RenderingUtils.renderStar(matrixStackIn, bufferIn, tileEntityIn.getLevel().getLevelData().getDayTime(), (int) (250 / distance),
-				tileEntityIn.getLevel().random.nextFloat() * 0.2f + 0.2f, 0, 0, 1, false);
-		RenderingUtils.renderStar(matrixStackIn, bufferIn, tileEntityIn.getLevel().getLevelData().getDayTime() + 20f, (int) (250 / distance),
-				tileEntityIn.getLevel().random.nextFloat() * 0.1f + 0.4f, 0, 0, 1, false);
-		RenderingUtils.renderStar(matrixStackIn, bufferIn, tileEntityIn.getLevel().getLevelData().getDayTime() + 40f, (int) (250 / distance),
-				tileEntityIn.getLevel().random.nextFloat() * 0.3f + 0.5f, 0, 0, 1, false);
+		RenderingUtils.renderStar(matrixStackIn, bufferIn, tileEntityIn.getLevel().getLevelData().getDayTime() + partialTicks, (int) (250 / distance),
+				tileEntityIn.getLevel().random.nextFloat() * 0.2f + 0.1f, 0, 0, 1f, false);
+		RenderingUtils.renderStar(matrixStackIn, bufferIn, tileEntityIn.getLevel().getLevelData().getDayTime() + 20f + partialTicks,
+				(int) (250 / distance), tileEntityIn.getLevel().random.nextFloat() * 0.1f + 0.3f, 0, 0, 0.6f, false);
+		RenderingUtils.renderStar(matrixStackIn, bufferIn, tileEntityIn.getLevel().getLevelData().getDayTime() + 40f + partialTicks,
+				(int) (250 / distance), tileEntityIn.getLevel().random.nextFloat() * 0.3f + 0.5f, 0, 0, 0.2f, false);
 		matrixStackIn.popPose();
 	}
 
