a11b43ea9a1ec03449c01c9c14d0994574c40423
aurilisdev <aurilisdev@gmail.com>
2022-04-28 19:42:52 +0200

    Fix some slot issues

--- src/main/java/nuclearscience/common/tile/TileAtomicAssembler.java
+++ src/main/java/nuclearscience/common/tile/TileAtomicAssembler.java
@@ -29,7 +29,8 @@ public class TileAtomicAssembler extends GenericTile {
 		addComponent(new ComponentTickable().tickServer(this::tickServer).tickCommon(this::tickCommon));
 		addComponent(new ComponentPacketHandler().guiPacketWriter(this::writeGuiPacket).guiPacketReader(this::readGuiPacket));
 		addComponent(new ComponentElectrodynamic(this).maxJoules(Constants.ATOMICASSEMBLER_USAGE_PER_TICK * 20).voltage(Constants.ATOMICASSEMBLER_VOLTAGE).input(Direction.DOWN));
-		addComponent(new ComponentInventory(this).size(8).slotFaces(0, Direction.values()).valid((slot, stack, i) -> slot == 6 || slot < 6 && stack.is(DeferredRegisters.ITEM_CELLDARKMATTER.get())).shouldSendInfo());
+		// The slot == 6 has to be there to allow items into the input slot.
+		addComponent(new ComponentInventory(this).size(8).faceSlots(Direction.UP, 0, 1, 2, 3, 4, 5, 6).slotFaces(6, Direction.DOWN, Direction.WEST, Direction.SOUTH, Direction.NORTH, Direction.EAST).valid((slot, stack, i) -> slot == 6 || slot < 6 && stack.is(DeferredRegisters.ITEM_CELLDARKMATTER.get())).shouldSendInfo());
 		addComponent(new ComponentContainerProvider("container.atomicassembler").createMenu((id, player) -> new ContainerAtomicAssembler(id, player, getComponent(ComponentType.Inventory), getCoordsArray())));
 	}
 
