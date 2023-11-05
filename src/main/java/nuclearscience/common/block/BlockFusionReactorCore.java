3849984333df705c2a92b94013358deec1810b00
skip999 <skiprocks999@gmail.com>
2023-11-05 13:52:56 -0600

    polish voxel shapes
    add proper voxelshapes for several machines

--- src/main/java/nuclearscience/common/block/BlockFusionReactorCore.java
+++ src/main/java/nuclearscience/common/block/BlockFusionReactorCore.java
@@ -1,8 +1,14 @@
 package nuclearscience.common.block;
 
 import electrodynamics.prefab.block.GenericMachineBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import nuclearscience.common.tile.fusionreactor.TileFusionReactorCore;
 
 public class BlockFusionReactorCore extends GenericMachineBlock {
@@ -14,6 +20,21 @@ public class BlockFusionReactorCore extends GenericMachineBlock {
 	@Override
 	public RenderShape getRenderShape(BlockState state) {
 		return RenderShape.MODEL;
+	}
+	
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+		VoxelShape reactor = Block.box(6, 0, 6, 10, 16, 10);
+		
+		reactor = Shapes.or(reactor, Block.box(5, 1, 5, 11, 15, 11));
+		
+		reactor = Shapes.or(reactor, Block.box(4, 2, 4, 12, 14, 12));
+		
+		reactor = Shapes.or(reactor, Block.box(3, 4, 3, 13, 12, 13));
+		
+		reactor = Shapes.or(reactor, Block.box(2, 6, 2, 14, 10, 14));
+		
+		return reactor;
 	}
 
 }
--- src/main/java/nuclearscience/common/block/voxelshapes/NuclearScienceVoxelShapeRegistry.java
+++ src/main/java/nuclearscience/common/block/voxelshapes/NuclearScienceVoxelShapeRegistry.java
@@ -17,6 +17,73 @@ public class NuclearScienceVoxelShapeRegistry {
 	 */
 	public static void init() {
 
+		/* CHEMICAL EXTRACTOR */
+
+		VoxelShape chemicalextractor = Block.box(0, 0, 0, 16, 2, 16);
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(5, 2, 0, 11, 14, 16));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 3, 2.5, 5, 4, 4.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 3, 5.5, 5, 4, 7.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 3, 8.5, 5, 4, 10.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 3, 11.5, 5, 4, 13.5));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2.5, 4, 3, 3.5, 4.75, 4));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2.5, 4, 6, 3.5, 4.75, 7));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2.5, 4, 9, 3.5, 4.75, 10));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2.5, 4, 12, 3.5, 4.75, 13));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 4.75, 2.25, 5, 6, 4.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 4.75, 5.25, 5, 6, 7.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 4.75, 8.25, 5, 6, 10.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 4.75, 11.25, 5, 6, 13.75));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 6, 2.5, 4, 11.75, 4.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 6, 5.5, 4, 11.75, 7.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 6, 8.5, 4, 11.75, 10.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(2, 6, 11.5, 4, 11.75, 13.5));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 11.75, 2.25, 5, 13, 4.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 11.75, 5.25, 5, 13, 7.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 11.75, 8.25, 5, 13, 10.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(1.75, 11.75, 11.25, 5, 13, 13.75));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(3.5, 13, 2.5, 5, 14, 4.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(3.5, 13, 5.5, 5, 14, 7.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(3.5, 13, 8.5, 5, 14, 10.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(3.5, 13, 11.5, 5, 14, 13.5));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 3, 2.5, 14, 4, 4.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 3, 5.5, 14, 4, 7.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 3, 8.5, 14, 4, 10.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 3, 11.5, 14, 4, 13.5));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12.5, 4, 3, 13.5, 4.75, 4));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12.5, 4, 6, 13.5, 4.75, 7));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12.5, 4, 9, 13.5, 4.75, 10));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12.5, 4, 12, 13.5, 4.75, 13));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 4.75, 2.25, 14.25, 6, 4.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 4.75, 5.25, 14.25, 6, 7.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 4.75, 8.25, 14.25, 6, 10.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 4.75, 11.25, 14.25, 6, 13.75));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12, 6, 2.5, 14, 11.75, 4.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12, 6, 5.5, 14, 11.75, 7.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12, 6, 8.5, 14, 11.75, 10.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(12, 6, 11.5, 14, 11.75, 13.5));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 11.75, 2.25, 14.25, 13, 4.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 11.75, 5.25, 14.25, 13, 7.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 11.75, 8.25, 14.25, 13, 10.75));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 11.75, 11.25, 14.25, 13, 13.75));
+
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 13, 2.5, 12.5, 14, 4.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 13, 5.5, 12.5, 14, 7.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 13, 8.5, 12.5, 14, 10.5));
+		chemicalextractor = Shapes.or(chemicalextractor, Block.box(11, 13, 11.5, 12.5, 14, 13.5));
+
+		VoxelShapes.registerShape(NuclearScienceBlocks.blockChemicalExtractor, chemicalextractor, Direction.EAST);
+
 		/* FISSION REACTOR CORE */
 
 		VoxelShape fissionreactorcore = Shapes.empty();
@@ -334,6 +401,32 @@ public class NuclearScienceVoxelShapeRegistry {
 		moltensaltsupplier = Shapes.join(moltensaltsupplier, Shapes.box(0.1875, 0.0625, 0.625, 0.25, 0.125, 0.6875), BooleanOp.OR);
 		VoxelShapes.registerShape(NuclearScienceBlocks.blockMoltenSaltSupplier, moltensaltsupplier, Direction.EAST);
 
+		/* NUCLEAR BOILER */
+
+		VoxelShape nuclearboiler = Block.box(0, 0, 0, 16, 2, 16);
+
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(7, 2, 0, 9, 6, 1.5));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(6, 6, 0, 10, 10, 1.5));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(7, 9.25, 0.25, 9, 11, 4.5));
+
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(7, 2, 14.5, 9, 6, 16));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(6, 6, 14.5, 10, 10, 16));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(7, 9.25, 11.5, 9, 11, 15.75));
+
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(2, 2, 2, 14, 3, 14));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(3.75, 3, 4, 12, 5, 12));
+
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(13, 2, 4.5, 16, 12, 11.5));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(10, 9, 5.5, 13, 11, 10.5));
+
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(7, 2, 5, 9, 8, 6.5));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(6, 8, 4.5, 10, 12, 7.5));
+
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(7, 2, 9.5, 9, 8, 11));
+		nuclearboiler = Shapes.or(nuclearboiler, Block.box(6, 8, 8.5, 10, 12, 11.5));
+
+		VoxelShapes.registerShape(NuclearScienceBlocks.blockNuclearBoiler, nuclearboiler, Direction.WEST);
+
 		/* PARTICLE INJECTOR */
 
 		VoxelShape particleinjector = Stream.of(
@@ -367,6 +460,139 @@ public class NuclearScienceVoxelShapeRegistry {
 		).reduce((v1, v2) -> Shapes.or(v1, v2)).get();
 
 		VoxelShapes.registerShape(NuclearScienceBlocks.blockParticleInjector, particleinjector, Direction.WEST);
+		
+		/* RADIOISOTROPIC GENERATOR */
+		
+		VoxelShape radiogenerator = Block.box(6, 0.25, 4, 10, 15.75, 12);
+		radiogenerator = Shapes.or(radiogenerator, Block.box(4, 0.25, 6, 12, 15.75, 10));
+		radiogenerator = Shapes.or(radiogenerator, Block.box(5, 0.25, 5, 11, 15.75, 11));
+
+		radiogenerator = Shapes.or(radiogenerator, Block.box(5.25, 0, 5.25, 10.75, 0.25, 10.75));
+		radiogenerator = Shapes.or(radiogenerator, Block.box(5.25, 15.75, 5.25, 10.75, 16, 10.75));
+		
+		VoxelShapes.registerShape(NuclearScienceBlocks.blockRadioisotopeGenerator, radiogenerator, Direction.EAST);
+
+		/* TELEPORTER */
+
+		VoxelShape teleporter = Stream.of(
+				//
+				Block.box(2, 1, 2, 14, 5, 14),
+				//
+				Block.box(0, 2, 14, 2, 5, 16),
+				//
+				Block.box(14, 2, 14, 16, 5, 16),
+				//
+				Block.box(14, 2, 0, 16, 5, 2),
+				//
+				Block.box(0, 2, 0, 2, 5, 2),
+				//
+				Block.box(0, 5, 0, 16, 7, 2),
+				//
+				Block.box(0, 0, 0, 16, 2, 2),
+				//
+				Block.box(0, 5, 14, 16, 7, 16),
+				//
+				Block.box(0, 0, 14, 16, 2, 16),
+				//
+				Block.box(0, 5, 2, 2, 7, 14),
+				//
+				Block.box(0, 0, 2, 2, 2, 14),
+				//
+				Block.box(14, 5, 2, 16, 7, 14),
+				//
+				Block.box(14, 0, 2, 16, 2, 14),
+				//
+				Block.box(7, 7, 1, 9, 8, 2),
+				//
+				Block.box(7, 7, 14, 9, 8, 15),
+				//
+				Block.box(7, 8, 14, 9, 9, 15),
+				//
+				Block.box(7, 8, 1, 9, 9, 2),
+				//
+				Block.box(7, 9, 2, 9, 10, 3),
+				//
+				Block.box(7, 9, 13, 9, 10, 14),
+				//
+				Block.box(7, 10, 13, 9, 11, 14),
+				//
+				Block.box(7, 10, 12, 9, 11, 13),
+				//
+				Block.box(7, 10, 3, 9, 11, 4),
+				//
+				Block.box(7, 10, 2, 9, 11, 3),
+				//
+				Block.box(1, 7, 7, 2, 8, 9),
+				//
+				Block.box(14, 7, 7, 15, 8, 9),
+				//
+				Block.box(1, 8, 7, 2, 9, 9),
+				//
+				Block.box(14, 8, 7, 15, 9, 9),
+				//
+				Block.box(2, 9, 7, 3, 10, 9),
+				//
+				Block.box(13, 9, 7, 14, 10, 9),
+				//
+				Block.box(2, 10, 7, 3, 11, 9),
+				//
+				Block.box(13, 10, 7, 14, 11, 9),
+				//
+				Block.box(3, 10, 7, 4, 11, 9),
+				//
+				Block.box(12, 10, 7, 13, 11, 9),
+				//
+				Block.box(3, 11, 3, 13, 12, 13),
+				//
+				Block.box(4, 12, 4, 12, 13, 12),
+				//
+				Block.box(5, 13, 5, 11, 14, 11),
+				//
+				Block.box(6, 5, 6, 10, 15, 10),
+				//
+				Block.box(3, 5, 3, 4, 11, 4),
+				//
+				Block.box(12, 5, 3, 13, 11, 4),
+				//
+				Block.box(3, 5, 12, 4, 11, 13),
+				//
+				Block.box(12, 5, 12, 13, 11, 13),
+				//
+				Block.box(1, 7, 1, 3, 8, 4),
+				//
+				Block.box(1, 7, 3, 3, 8, 4),
+				//
+				Block.box(3, 7, 1, 4, 8, 3),
+				//
+				Block.box(13, 7, 12, 15, 8, 15),
+				//
+				Block.box(1, 7, 12, 3, 8, 13),
+				//
+				Block.box(1, 7, 13, 4, 8, 15),
+				//
+				Block.box(12, 7, 13, 13, 8, 15),
+				//
+				Block.box(12, 7, 1, 15, 8, 3),
+				//
+				Block.box(13, 7, 3, 15, 8, 4),
+				//
+				Block.box(0, 15, 0, 16, 16, 16),
+				//
+				Block.box(4, 0, 4, 12, 1, 12),
+				//
+				Block.box(3, 12, 3, 4, 15, 4),
+				//
+				Block.box(12, 12, 3, 13, 15, 4),
+				//
+				Block.box(3, 12, 12, 4, 15, 13),
+				//
+				Block.box(12, 12, 12, 13, 15, 13)
+		//
+		).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
+
+		VoxelShapes.registerShape(NuclearScienceBlocks.blockTeleporter, teleporter, Direction.UP);
+		
+		
 
 	}
 
