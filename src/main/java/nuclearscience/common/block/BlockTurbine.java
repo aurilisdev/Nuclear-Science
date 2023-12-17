1f61bb1812ffec03351721fcee1e316350cb64bd
skip999 <skiprocks999@gmail.com>
2023-12-17 11:30:10 -0600

    fix dupe with turbine

--- src/main/java/nuclearscience/common/block/BlockTurbine.java
+++ src/main/java/nuclearscience/common/block/BlockTurbine.java
@@ -26,6 +26,8 @@ import nuclearscience.common.tile.TileTurbine;
 
 public class BlockTurbine extends GenericEntityBlockWaterloggable {
 
+	private static final VoxelShape SHAPE = Shapes.or(Block.box(1.25, 2.5, 6, 14.75, 13.5, 10), Block.box(6, 2.5, 1.25, 10, 13.5, 14.75), Block.box(2, 2.5, 4, 14, 13.5, 12), Block.box(4, 2.5, 2, 12, 13.5, 14), Block.box(3, 2.5, 3, 13, 13.5, 13), Block.box(4.65, 0.75, 4.65, 11.35, 2.5, 11.35), Block.box(4.3, 13.5, 4.3, 11.7, 15, 11.7), Block.box(5.7, 15, 5.7, 10.3, 16, 10.3));
+
 	public static final BooleanProperty RENDER = BooleanProperty.create("render");
 
 	public BlockTurbine() {
@@ -52,17 +54,6 @@ public class BlockTurbine extends GenericEntityBlockWaterloggable {
 				turbine.constructStructure();
 			}
 		}
-	}
-
-	@Override
-	public void onRemove(BlockState state, Level worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-		if (state.getBlock() != newState.getBlock() && !worldIn.isClientSide) {
-			TileTurbine turbine = (TileTurbine) worldIn.getBlockEntity(pos);
-			if (turbine != null) {
-				turbine.deconstructStructure();
-			}
-		}
-		super.onRemove(state, worldIn, pos, newState, isMoving);
 	}
 
 	@Override
@@ -101,18 +92,7 @@ public class BlockTurbine extends GenericEntityBlockWaterloggable {
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
 		if (state.getValue(RENDER)) {
-			VoxelShape turbine = Block.box(1.25, 2.5, 6, 14.75, 13.5, 10);
-			turbine = Shapes.or(turbine, Block.box(6, 2.5, 1.25, 10, 13.5, 14.75));
-
-			turbine = Shapes.or(turbine, Block.box(2, 2.5, 4, 14, 13.5, 12));
-			turbine = Shapes.or(turbine, Block.box(4, 2.5, 2, 12, 13.5, 14));
-			turbine = Shapes.or(turbine, Block.box(3, 2.5, 3, 13, 13.5, 13));
-
-			turbine = Shapes.or(turbine, Block.box(4.65, 0.75, 4.65, 11.35, 2.5, 11.35));
-			turbine = Shapes.or(turbine, Block.box(4.3, 13.5, 4.3, 11.7, 15, 11.7));
-			turbine = Shapes.or(turbine, Block.box(5.7, 15, 5.7, 10.3, 16, 10.3));
-
-			return turbine;
+			return SHAPE;
 		}
 
 		return Shapes.block();
