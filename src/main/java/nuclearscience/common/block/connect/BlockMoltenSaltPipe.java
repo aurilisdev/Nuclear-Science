6c483fcf9d6137edc49aeebb9916daab926264fd
skip999 <skiprocks999@gmail.com>
2024-07-04 10:37:19 -0500

    update pipe formatting

--- src/main/java/nuclearscience/common/block/connect/BlockMoltenSaltPipe.java
+++ src/main/java/nuclearscience/common/block/connect/BlockMoltenSaltPipe.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import electrodynamics.api.network.cable.IRefreshableCable;
 import electrodynamics.common.block.connect.util.AbstractRefreshingConnectBlock;
 import electrodynamics.common.block.connect.util.EnumConnectType;
+import electrodynamics.prefab.tile.types.GenericConnectTile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
@@ -13,7 +14,6 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
 import nuclearscience.api.network.moltensalt.IMoltenSaltPipe;
 import nuclearscience.common.block.subtype.SubtypeMoltenSaltPipe;
 import nuclearscience.common.tile.msreactor.TileHeatExchanger;
@@ -51,17 +51,18 @@ public class BlockMoltenSaltPipe extends AbstractRefreshingConnectBlock {
 	}
 
 	@Override
-	public BlockState refreshConnections(BlockState otherState, BlockEntity tile, BlockState state, Direction dir) {
-		EnumProperty<EnumConnectType> property = FACING_TO_PROPERTY_MAP.get(dir);
-		if (tile instanceof IMoltenSaltPipe) {
-			return state.setValue(property, EnumConnectType.WIRE);
+	public BlockState refreshConnections(BlockState otherState, BlockEntity otherTile, BlockState state, BlockEntity thisTile, Direction dir) {
+		if(!(thisTile instanceof GenericConnectTile)) {
+			return state;
 		}
-		if (tile instanceof TileMSReactorCore && dir.getOpposite() == Direction.UP || tile instanceof TileHeatExchanger && dir.getOpposite() == Direction.DOWN) {
-			return state.setValue(property, EnumConnectType.INVENTORY);
+		GenericConnectTile thisConnect = (GenericConnectTile) thisTile;
+		EnumConnectType connection = EnumConnectType.NONE;
+		if (otherTile instanceof IMoltenSaltPipe) {
+			connection = EnumConnectType.WIRE;
+		} else if ((otherTile instanceof TileMSReactorCore && dir.getOpposite() == Direction.UP) || (otherTile instanceof TileHeatExchanger && dir.getOpposite() == Direction.DOWN)) {
+			connection = EnumConnectType.INVENTORY;
 		}
-		if (state.hasProperty(property)) {
-			return state.setValue(property, EnumConnectType.NONE);
-		}
+		thisConnect.writeConnection(dir, connection);
 		return state;
 	}
 
