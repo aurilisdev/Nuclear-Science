7a938bc88ffc6756b7b3654f49a34fb188216ff8
aurilisdev <aurilisdev@gmail.com>
2025-05-02 15:07:39 +0200

    Fix render bug? (might be wrong)

--- src/main/java/nuclearscience/client/render/tile/RenderFissionInterface.java
+++ src/main/java/nuclearscience/client/render/tile/RenderFissionInterface.java
@@ -319,7 +319,8 @@ public class RenderFissionInterface extends AbstractTileRenderer<TileFissionInte
                     }
 
 
-                case FISSION_TRITIUM_EXTRACT:
+                    break;
+	    case FISSION_TRITIUM_EXTRACT:
 
                     if (perc > 0.5) {
 
@@ -383,7 +384,7 @@ public class RenderFissionInterface extends AbstractTileRenderer<TileFissionInte
         matrix.popPose();
     }
 
-    private void renderBox(PoseStack matrix, AABB box, Color color, TextureAtlasSprite texture, MultiBufferSource bufferIn, int combinedLightIn, boolean[] faces) {
+    private static void renderBox(PoseStack matrix, AABB box, Color color, TextureAtlasSprite texture, MultiBufferSource bufferIn, int combinedLightIn, boolean[] faces) {
         RenderingUtils.renderFilledBoxNoOverlay(matrix, bufferIn.getBuffer(RenderType.SOLID), box, color.rFloat(), color.gFloat(), color.bFloat(), color.aFloat(), texture.getU0(), texture.getV0(), texture.getU1(), texture.getV1(), combinedLightIn, faces);
     }
 
