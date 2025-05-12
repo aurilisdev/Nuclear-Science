d15f51b351213cc88041c2f7a88777383aee9607
skip999 <skiprocks999@gmail.com>
2025-05-11 21:20:18 -0500

    add color to gases

--- src/main/java/nuclearscience/registers/NuclearScienceGases.java
+++ src/main/java/nuclearscience/registers/NuclearScienceGases.java
@@ -6,12 +6,13 @@ import net.minecraftforge.registries.RegistryObject;
 import nuclearscience.NuclearScience;
 import nuclearscience.prefab.utils.NuclearTextUtils;
 import voltaic.api.gas.Gas;
+import voltaic.prefab.utilities.math.Color;
 import voltaic.registers.VoltaicRegistries;
 
 public class NuclearScienceGases {
 
 	public static final DeferredRegister<Gas> GASES = DeferredRegister.create(VoltaicRegistries.GAS_REGISTRY_KEY, NuclearScience.ID);
 
-	public static final RegistryObject<Gas> URANIUM_HEXAFLUORIDE = GASES.register("uraniumhexafluoride", () -> new Gas(() -> Items.AIR, NuclearTextUtils.gas("uraniumhexafluoride")));
+	public static final RegistryObject<Gas> URANIUM_HEXAFLUORIDE = GASES.register("uraniumhexafluoride", () -> new Gas(() -> Items.AIR, NuclearTextUtils.gas("uraniumhexafluoride"), Color.WHITE));
 
 }
