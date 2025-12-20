2829a711cbca8c6c8f59798bf9c02ca6da20cb30
aurilisdev <aurilisdev@gmail.com>
2025-12-21 00:50:19 +0100

    Make petrified wood flammable

--- src/main/java/nuclearscience/common/block/subtype/SubtypeIrradiatedBlock.java
+++ src/main/java/nuclearscience/common/block/subtype/SubtypeIrradiatedBlock.java
@@ -8,12 +8,21 @@ import voltaic.api.ISubtype;
 public enum SubtypeIrradiatedBlock implements ISubtype {
     soil(Properties.copy(Blocks.DIRT).randomTicks()),
     grass(Properties.copy(Blocks.GRASS_BLOCK).randomTicks()),
-    petrifiedwood(Properties.copy(Blocks.OAK_WOOD).randomTicks());
+    petrifiedwood(Properties.copy(Blocks.OAK_WOOD).randomTicks(), true);
 
     public final BlockBehaviour.Properties properties;
 
+    private boolean burnable = false;
+
     private SubtypeIrradiatedBlock(BlockBehaviour.Properties properties) {
         this.properties = properties;
+    }
+    private SubtypeIrradiatedBlock(BlockBehaviour.Properties properties, boolean burnable) {
+	this(properties);
+	this.burnable = burnable;
+    }    
+    public boolean burnable() {
+	return burnable;
     }
 
     @Override
