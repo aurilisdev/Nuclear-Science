2f9fbe9969127adc39031888e9e716bb44b160a2
aurilisdev <aurilisdev@gmail.com>
2026-08-03 13:39:16 +0200

    Patch 9: Fix wrongly copied code for atomic assembler...

--- src/main/java/nuclearscience/common/tile/TileAtomicAssembler.java
+++ src/main/java/nuclearscience/common/tile/TileAtomicAssembler.java
@@ -26,110 +26,125 @@ import voltaic.prefab.utilities.RadiationUtils;
 
 public class TileAtomicAssembler extends GenericTile {
 
-	public final SingleProperty<Integer> progress = property(new SingleProperty<>(PropertyTypes.INTEGER, "progress", 0));
+    public final SingleProperty<Integer> progress = property(new SingleProperty<>(PropertyTypes.INTEGER, "progress", 0))
+	    .setNoUpdateServer();
 
-	public TileAtomicAssembler(BlockPos pos, BlockState state) {
-		super(NuclearScienceTiles.TILE_ATOMICASSEMBLER.get(), pos, state);
+    public TileAtomicAssembler(BlockPos pos, BlockState state) {
+	super(NuclearScienceTiles.TILE_ATOMICASSEMBLER.get(), pos, state);
 
-		addComponent(new ComponentTickable(this).tickCommon(this::tickServer));
-		addComponent(new ComponentPacketHandler(this));
-		addComponent(new ComponentElectrodynamic(this, false, true).maxJoules(NuclearConstants.ATOMICASSEMBLER_USAGE_PER_TICK * 20).voltage(NuclearConstants.ATOMICASSEMBLER_VOLTAGE).setInputDirections(BlockEntityUtils.MachineDirection.BOTTOM));
-		// The slot == 6 has to be there to allow items into the input slot.
-		addComponent(new ComponentInventory(this, ComponentInventory.InventoryBuilder.newInv().inputs(7).outputs(1)).setSlotsByDirection(BlockEntityUtils.MachineDirection.TOP, 0, 1, 2, 3, 4, 5).setDirectionsBySlot(6, BlockEntityUtils.MachineDirection.RIGHT, BlockEntityUtils.MachineDirection.BACK)
-				//
-				.setDirectionsBySlot(7, BlockEntityUtils.MachineDirection.LEFT, BlockEntityUtils.MachineDirection.FRONT).valid((slot, stack, i) -> slot == 6 || slot < 6 && stack.is(NuclearScienceItems.ITEM_CELLDARKMATTER.get())));
-		addComponent(new ComponentContainerProvider("atomicassembler", this).createMenu((id, player) -> new ContainerAtomicAssembler(id, player, getComponent(IComponentType.Inventory), getCoordsArray())));
+	addComponent(new ComponentTickable(this).tickServer(this::tickServer));
+	addComponent(new ComponentPacketHandler(this));
+	addComponent(new ComponentElectrodynamic(this, false, true)
+		.maxJoules(NuclearConstants.ATOMICASSEMBLER_USAGE_PER_TICK * 20)
+		.voltage(NuclearConstants.ATOMICASSEMBLER_VOLTAGE)
+		.setInputDirections(BlockEntityUtils.MachineDirection.BOTTOM));
+	// The slot == 6 has to be there to allow items into the input slot.
+	addComponent(new ComponentInventory(this, ComponentInventory.InventoryBuilder.newInv().inputs(7).outputs(1))
+		.setSlotsByDirection(BlockEntityUtils.MachineDirection.TOP, 0, 1, 2, 3, 4, 5)
+		.setDirectionsBySlot(6, BlockEntityUtils.MachineDirection.RIGHT, BlockEntityUtils.MachineDirection.BACK)
+		//
+		.setDirectionsBySlot(7, BlockEntityUtils.MachineDirection.LEFT, BlockEntityUtils.MachineDirection.FRONT)
+		.valid((slot, stack, i) -> slot == 6
+			|| slot < 6 && stack.is(NuclearScienceItems.ITEM_CELLDARKMATTER.get())));
+	addComponent(new ComponentContainerProvider("atomicassembler", this)
+		.createMenu((id, player) -> new ContainerAtomicAssembler(id, player,
+			getComponent(IComponentType.Inventory), getCoordsArray())));
+    }
+
+    private void tickServer(ComponentTickable tickable) {
+
+	ComponentInventory inv = getComponent(IComponentType.Inventory);
+
+	ItemStack input = inv.getItem(6);
+
+	RadiationUtils.handleRadioactiveItems(this, inv, NuclearConstants.ATOMIC_ASSEMBLER_RADIATION_RADIUS, true, 30,
+		true, false);
+
+	if (input.isEmpty()) {
+	    progress.setValue(0);
+	    return;
 	}
 
-	private void tickServer(ComponentTickable tickable) {
+	ItemStack output = inv.getItem(7);
 
-		ComponentInventory inv = getComponent(IComponentType.Inventory);
+	boolean validItem = validateDupeItem(input) && (output.isEmpty()
+		|| ItemStack.isSameItemSameTags(input, output) && output.getCount() + 1 <= output.getMaxStackSize());
 
-		ItemStack input = inv.getItem(6);
-		
-		RadiationUtils.handleRadioactiveItems(this, inv, NuclearConstants.ATOMIC_ASSEMBLER_RADIATION_RADIUS, true, 30, true, false);
+	if (!validItem) {
+	    progress.setValue(0);
+	    return;
+	}
 
-		if (input.isEmpty()) {
-			progress.setValue(0);
-			return;
-		}
+	for (int index = 0; index < 6; index++) {
 
-		ItemStack output = inv.getItem(7);
+	    ItemStack dmCell = inv.getItem(index);
 
-		boolean validItem = validateDupeItem(input) && (output.isEmpty() || ItemStack.isSameItemSameTags(input, output) && output.getCount() + 1 <= output.getMaxStackSize());
-
-		if (!validItem) {
-			progress.setValue(0);
-			return;
-		}
-
-		for (int index = 0; index < 6; index++) {
-
-			ItemStack dmCell = inv.getItem(index);
-
-			if (dmCell.isEmpty() || dmCell.getItem() != NuclearScienceItems.ITEM_CELLDARKMATTER.get()) {
-				progress.setValue(0);
-				return;
-			}
-
-			if (dmCell.getDamageValue() >= dmCell.getMaxDamage()) {
-				progress.setValue(0);
-				inv.setItem(index, ItemStack.EMPTY);
-				return;
-			}
-
-		}
-
-		ComponentElectrodynamic electro = getComponent(IComponentType.Electrodynamic);
-
-		if (electro.getJoulesStored() < NuclearConstants.ATOMICASSEMBLER_USAGE_PER_TICK) {
-			return;
-		}
-
-		progress.setValue(progress.getValue() + 1);
-
-		electro.joules(electro.getJoulesStored() - NuclearConstants.ATOMICASSEMBLER_USAGE_PER_TICK);
-
-		if (progress.getValue() < NuclearConstants.ATOMICASSEMBLER_REQUIRED_TICKS) {
-			return;
-		}
-
+	    if (dmCell.isEmpty() || dmCell.getItem() != NuclearScienceItems.ITEM_CELLDARKMATTER.get()) {
 		progress.setValue(0);
+		return;
+	    }
 
-		for (int index = 0; index < 6; index++) {
-
-			ItemStack dmCell = inv.getItem(index);
-
-			dmCell.setDamageValue(dmCell.getDamageValue() + 1);
-
-			if (dmCell.getDamageValue() >= dmCell.getMaxDamage()) {
-				inv.setItem(index, ItemStack.EMPTY);
-			}
-		}
-
-		if (output.isEmpty()) {
-
-			inv.setItem(7, new ItemStack(input.getItem()));
-
-		} else {
-
-			output.setCount(output.getCount() + 1);
-
-		}
-	}
-
-	private boolean validateDupeItem(ItemStack stack) {
-
-		if(AtomicAssemblerWhitelistRegister.INSTANCE.isWhitelisted(stack.getItem())) {
-			return true;
-		}
-
-		if (AtomicAssemblerBlacklistRegister.INSTANCE.isBlacklisted(stack.getItem()) || stack.hasTag() || (ItemUtils.testItems(stack.getItem(), NuclearScienceItems.ITEM_CELLDARKMATTER.get()) && stack.getCapability(ForgeCapabilities.ITEM_HANDLER).orElse(CapabilityUtils.EMPTY_ITEM_HANDLER) != CapabilityUtils.EMPTY_ITEM_HANDLER)) {
-			return false;
-		}
-
-		return true;
+	    if (dmCell.getDamageValue() >= dmCell.getMaxDamage()) {
+		progress.setValue(0);
+		inv.setItem(index, ItemStack.EMPTY);
+		return;
+	    }
 
 	}
+
+	ComponentElectrodynamic electro = getComponent(IComponentType.Electrodynamic);
+
+	if (electro.getJoulesStored() < NuclearConstants.ATOMICASSEMBLER_USAGE_PER_TICK) {
+	    return;
+	}
+
+	progress.setValue(progress.getValue() + 1);
+
+	electro.joules(electro.getJoulesStored() - NuclearConstants.ATOMICASSEMBLER_USAGE_PER_TICK);
+
+	if (progress.getValue() < NuclearConstants.ATOMICASSEMBLER_REQUIRED_TICKS) {
+	    return;
+	}
+
+	progress.setValue(0);
+
+	for (int index = 0; index < 6; index++) {
+
+	    ItemStack dmCell = inv.getItem(index);
+
+	    dmCell.setDamageValue(dmCell.getDamageValue() + 1);
+
+	    if (dmCell.getDamageValue() >= dmCell.getMaxDamage()) {
+		inv.setItem(index, ItemStack.EMPTY);
+	    }
+	}
+
+	if (output.isEmpty()) {
+
+	    inv.setItem(7, new ItemStack(input.getItem()));
+
+	} else {
+
+	    output.setCount(output.getCount() + 1);
+
+	}
+    }
+
+    private boolean validateDupeItem(ItemStack stack) {
+
+	if (AtomicAssemblerWhitelistRegister.INSTANCE.isWhitelisted(stack.getItem())) {
+	    return true;
+	}
+
+	if (AtomicAssemblerBlacklistRegister.INSTANCE.isBlacklisted(stack.getItem()) || stack.hasTag()
+		|| (ItemUtils.testItems(stack.getItem(), NuclearScienceItems.ITEM_CELLDARKMATTER.get())
+			&& stack.getCapability(ForgeCapabilities.ITEM_HANDLER)
+				.orElse(CapabilityUtils.EMPTY_ITEM_HANDLER) != CapabilityUtils.EMPTY_ITEM_HANDLER)) {
+	    return false;
+	}
+
+	return true;
+
+    }
 
 }
