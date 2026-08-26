be0ae698dfa6eb8d58d1ca90ba3fd4b6ee38f50a
aurilisdev <aurilisdev@gmail.com>
2026-08-26 20:27:13 +0200

    Patch 11: Fix #193: Block Entity Network Issues

--- src/main/java/nuclearscience/common/network/ReactorLogisticsNetwork.java
+++ src/main/java/nuclearscience/common/network/ReactorLogisticsNetwork.java
@@ -23,130 +23,120 @@ import voltaic.common.network.NetworkRegistry;
 import voltaic.prefab.network.AbstractNetwork;
 
 public class ReactorLogisticsNetwork extends
-		AbstractNetwork<TileReactorLogisticsCable, SubtypeReactorLogisticsCable, Void, ReactorLogisticsNetwork> {
+	AbstractNetwork<TileReactorLogisticsCable, SubtypeReactorLogisticsCable, Void, ReactorLogisticsNetwork> {
 
-	private TileController controller;
-	private final HashMap<BlockPos, TileControlRodModule> controlRods = new HashMap<>();
-	private final HashMap<BlockPos, TileSupplyModule> supplyModules = new HashMap<>();
-	private final HashMap<BlockPos, GenericTileInterface> interfaces = new HashMap<>();
-	private final HashMap<BlockPos, TileMonitorModule> monitors = new HashMap<>();
-	private final HashMap<BlockPos, TileThermometerModule> thermometers = new HashMap<>();
+    private TileController controller;
+    private final HashMap<BlockPos, TileControlRodModule> controlRods = new HashMap<>();
+    private final HashMap<BlockPos, TileSupplyModule> supplyModules = new HashMap<>();
+    private final HashMap<BlockPos, GenericTileInterface> interfaces = new HashMap<>();
+    private final HashMap<BlockPos, TileMonitorModule> monitors = new HashMap<>();
+    private final HashMap<BlockPos, TileThermometerModule> thermometers = new HashMap<>();
 
-	public ReactorLogisticsNetwork(Collection<TileReactorLogisticsCable> varCables) {
-		conductorSet.addAll(varCables);
-		NetworkRegistry.register(this);
+    public ReactorLogisticsNetwork(Collection<TileReactorLogisticsCable> varCables) {
+	conductorSet.addAll(varCables);
+	NetworkRegistry.register(this);
+    }
+
+    public ReactorLogisticsNetwork(Set<ReactorLogisticsNetwork> networks) {
+	for (ReactorLogisticsNetwork net : networks) {
+	    if (net != null) {
+		conductorSet.addAll(net.conductorSet);
+		net.deregister();
+	    }
 	}
+	NetworkRegistry.register(this);
+    }
 
-	public ReactorLogisticsNetwork(Set<ReactorLogisticsNetwork> networks) {
-		for (ReactorLogisticsNetwork net : networks) {
-			if (net != null) {
-				conductorSet.addAll(net.conductorSet);
-				net.deregister();
-			}
+    @Override
+    public void refreshNewNetwork() {
+	controller = null;
+	interfaces.clear();
+	controlRods.clear();
+	supplyModules.clear();
+	monitors.clear();
+	thermometers.clear();
+	super.refreshNewNetwork();
+    }
+
+    @Override
+    public void resetReceiverStatistics() {
+	super.resetReceiverStatistics();
+	controller = null;
+	interfaces.clear();
+	controlRods.clear();
+	supplyModules.clear();
+	monitors.clear();
+	thermometers.clear();
+    }
+
+    @Override
+    public void updateRecieverStatistics(BlockEntity reciever, Direction dir) {
+	if (reciever instanceof TileController controller) {
+	    this.controller = controller;
+	} else if (reciever instanceof GenericTileInterface reactorInterface) {
+	    interfaces.put(reactorInterface.getBlockPos(), reactorInterface);
+	} else if (reciever instanceof TileControlRodModule controlRod) {
+	    controlRods.put(controlRod.getBlockPos(), controlRod);
+	} else if (reciever instanceof TileSupplyModule supplyModule) {
+	    supplyModules.put(supplyModule.getBlockPos(), supplyModule);
+	} else if (reciever instanceof TileMonitorModule monitor) {
+	    monitors.put(monitor.getBlockPos(), monitor);
+	} else if (reciever instanceof TileThermometerModule thermometer) {
+	    thermometers.put(thermometer.getBlockPos(), thermometer);
+	}
+    }
+
+    @Override
+    public Void emit(Void transfer, ArrayList<BlockEntity> ignored, boolean debug) {
+	throw new UnsupportedOperationException("The Reactor Logistics Network does not emit, what are you doing?");
+    }
+
+    @Override
+    public boolean isConductor(BlockEntity blockEntity, TileReactorLogisticsCable iReactorLogisticsCable) {
+	return blockEntity instanceof TileReactorLogisticsCable;
+    }
+
+    @Override
+    public ReactorLogisticsNetwork createInstanceConductor(Set<TileReactorLogisticsCable> set) {
+	return new ReactorLogisticsNetwork(set);
+    }
+
+    @Nullable
+    public TileController getController() {
+	return controller;
+    }
+
+    @Nullable
+    public TileControlRodModule getControlRod(BlockPos pos) {
+	return controlRods.getOrDefault(pos, null);
+    }
+
+    @Nullable
+    public TileSupplyModule getSupplyModule(BlockPos pos) {
+	return supplyModules.getOrDefault(pos, null);
+    }
+
+    @Nullable
+    public GenericTileInterface getInterface(BlockPos pos) {
+	return interfaces.getOrDefault(pos, null);
+    }
+
+    public List<GenericTileInterface> getInterfacesForType(GenericTileInterface.InterfaceType... types) {
+	List<GenericTileInterface> interfaces = new ArrayList<>();
+
+	this.interfaces.forEach((pos, tile) -> {
+	    for (GenericTileInterface.InterfaceType type : types) {
+		if (tile.getInterfaceType() == type) {
+		    interfaces.add(tile);
 		}
-		NetworkRegistry.register(this);
-	}
+	    }
+	});
 
-	@Override
-	public void refreshNewNetwork() {
-		controller = null;
-		interfaces.clear();
-		controlRods.clear();
-		supplyModules.clear();
-		monitors.clear();
-		thermometers.clear();
-		super.refreshNewNetwork();
-	}
+	return interfaces;
+    }
 
-	@Override
-	public void resetReceiverStatistics() {
-		super.resetReceiverStatistics();
-		controller = null;
-		interfaces.clear();
-		controlRods.clear();
-		supplyModules.clear();
-		monitors.clear();
-		thermometers.clear();
-	}
-
-	@Override
-	public void updateRecieverStatistics(BlockEntity reciever, Direction dir) {
-		if (reciever instanceof TileController controller) {
-			this.controller = controller;
-		} else if (reciever instanceof GenericTileInterface reactorInterface) {
-			interfaces.put(reactorInterface.getBlockPos(), reactorInterface);
-		} else if (reciever instanceof TileControlRodModule controlRod) {
-			controlRods.put(controlRod.getBlockPos(), controlRod);
-		} else if (reciever instanceof TileSupplyModule supplyModule) {
-			supplyModules.put(supplyModule.getBlockPos(), supplyModule);
-		} else if (reciever instanceof TileMonitorModule monitor) {
-			monitors.put(monitor.getBlockPos(), monitor);
-		} else if (reciever instanceof TileThermometerModule thermometer) {
-			thermometers.put(thermometer.getBlockPos(), thermometer);
-		}
-	}
-	
-	@Override
-	public void tick() {
-		for(BlockEntity tile : acceptorSet) {
-			if(tile == null || tile.isRemoved()) {
-				acceptorSet.remove(tile);
-				acceptorInputMap.remove(tile);
-			}
-		}
-	}
-
-	@Override
-	public Void emit(Void transfer, ArrayList<BlockEntity> ignored, boolean debug) {
-		throw new UnsupportedOperationException("The Reactor Logistics Network does not emit, what are you doing?");
-	}
-
-	@Override
-	public boolean isConductor(BlockEntity blockEntity, TileReactorLogisticsCable iReactorLogisticsCable) {
-		return blockEntity instanceof TileReactorLogisticsCable;
-	}
-
-	@Override
-	public ReactorLogisticsNetwork createInstanceConductor(Set<TileReactorLogisticsCable> set) {
-		return new ReactorLogisticsNetwork(set);
-	}
-
-	@Nullable
-	public TileController getController() {
-		return controller;
-	}
-
-	@Nullable
-	public TileControlRodModule getControlRod(BlockPos pos) {
-		return controlRods.getOrDefault(pos, null);
-	}
-
-	@Nullable
-	public TileSupplyModule getSupplyModule(BlockPos pos) {
-		return supplyModules.getOrDefault(pos, null);
-	}
-
-	@Nullable
-	public GenericTileInterface getInterface(BlockPos pos) {
-		return interfaces.getOrDefault(pos, null);
-	}
-
-	public List<GenericTileInterface> getInterfacesForType(GenericTileInterface.InterfaceType... types) {
-		List<GenericTileInterface> interfaces = new ArrayList<>();
-
-		this.interfaces.forEach((pos, tile) -> {
-			for (GenericTileInterface.InterfaceType type : types) {
-				if (tile.getInterfaceType() == type) {
-					interfaces.add(tile);
-				}
-			}
-		});
-
-		return interfaces;
-	}
-
-	public boolean isControllerActive() {
-		return controller != null && controller.isActive();
-	}
+    public boolean isControllerActive() {
+	return controller != null && controller.isActive();
+    }
 
 }
